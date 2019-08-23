dbfa100391fbc5ccb38e61cfcf6424a03f6a766d
CircleCI <torbjorv@gmail.com>
2019-08-23 11:16:28 -0700

    add a basic library for polygon clipping and area calculations. WIll be used for the flares service

---
+++ src/app/polygontools.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+
+import * as p from 'polygon-tools';
+
+// no idea why this line is necessary
+const polygonTools: any = p;
+
+// just a few sanity checks to make sure the handmade typings file works.
+fdescribe('PolygonTools', () => {
+  beforeEach(() => TestBed.configureTestingModule({}));
+
+  it('test area', () => {
+
+    // given
+    const polygon = [[0, 0], [1, 1], [0, 1]];
+
+    // then
+    expect(polygonTools.polygon.area(polygon, null)).toEqual(0.5);
+  });
+
+  it('test subtract', () => {
+
+    // given
+    const rectangle1 = [[0, 0], [2, 0], [2, 2], [0, 2]];
+    const rectangle2 = [[1, 1], [3, 1], [3, 3], [1, 3]];
+
+    // then
+    const actual = polygonTools.polygon.subtract(rectangle1, rectangle2);
+    const expected = [[0, 2], [0, 0], [2, 0], [2, 1], [1, 1], [1, 2]];
+    expect(actual).toEqual([expected]);
+    expect(polygonTools.polygon.area(actual[0])).toEqual(3);
+  });
+});
---
+++ src/app/types/polygon-tools.d.ts
@@ -0,0 +1,47 @@
+
+declare module 'polygon-tools' {
+
+// gemerated by dts-gen. 
+// declare const polygon_tools: {
+//     polygon: {
+//         WINDING_CCW: number;
+//         WINDING_CW: number;
+//         WINDING_UNKNOWN: number;
+//         area: any;
+//         bounds: any;
+//         ccw: any;
+//         centroid: any;
+//         ensure_ccw: any;
+//         ensure_cw: any;
+//         intersection: any;
+//         is_ccw: any;
+//         is_cw: any;
+//         normal: any;
+//         subtract: any;
+//         triangulate: any;
+//         union: any;
+//         winding: any;
+//     };
+//     tesselator: {
+//         DEFAULT_OPTIONS: {
+//             autoWinding: boolean;
+//             boundaryOnly: boolean;
+//             holes: any[];
+//             normal: any;
+//             polygons: any[];
+//             windingRule: number;
+//         };
+//         GLU_TESS_WINDING_ABS_GEQ_TWO: number;
+//         GLU_TESS_WINDING_NEGATIVE: number;
+//         GLU_TESS_WINDING_NONZERO: number;
+//         GLU_TESS_WINDING_ODD: number;
+//         GLU_TESS_WINDING_POSITIVE: number;
+//         GL_LINE_LOOP: number;
+//         GL_TRIANGLES: number;
+//         GL_TRIANGLE_FAN: number;
+//         GL_TRIANGLE_STRIP: number;
+//         Tesselator: any;
+//         run: any;
+//     };
+// };
+}
