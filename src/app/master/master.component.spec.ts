d3b8886364f368a69be01f9b90abcf3a58ebd018
CircleCI <torbjorv@gmail.com>
2019-09-10 23:53:17 -0700

    fix test

--- src/app/master/master.component.spec.ts
+++ src/app/master/master.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MasterComponent } from './master.component';
+import { SystemJsNgModuleLoader } from '@angular/core';
 
 describe('MasterComponent', () => {
   let component: MasterComponent;
@@ -8,7 +9,9 @@ describe('MasterComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MasterComponent ]
+      declarations: [ MasterComponent ],
+      // tslint:disable-next-line: deprecation
+      providers: [SystemJsNgModuleLoader]
     })
     .compileComponents();
   }));
