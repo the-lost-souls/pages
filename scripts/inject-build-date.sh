16230dee87f0d8ff0266d7e417a37cf5f26edaf4
CircleCI <torbjorv@gmail.com>
2019-09-16 21:31:53 -0700

    show build date

--- scripts/inject-build-date.sh
+++ scripts/inject-build-date.sh
@@ -2,4 +2,4 @@
 set -e
 
 BUILD_DATE=`date +%d%b%Y`
-sed -i.bak 's/Build: local/Build: '"$BUILD_DATE"'/g' src/index.html
+sed -i.bak 's/Build: local/Build: '"$BUILD_DATE"'/g' src/app/main/main.component.html
